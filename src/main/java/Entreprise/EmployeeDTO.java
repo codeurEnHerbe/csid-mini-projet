3a9c8c9bdc02068649928617b6efdff2fdfaa849
RAFAEL AURIAC <rauriac@iut.univ-paris8.fr>
2020-01-23 14:27:38 +0100

    ignore

--- src/main/java/Entreprise/EmployeeDTO.java
+++ src/main/java/Entreprise/EmployeeDTO.java
@@ -1,5 +1,7 @@
 package Entreprise;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -8,43 +10,99 @@ public class EmployeeDTO {
 
     private int id;
     private String name;
-    private String address;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String streetNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String streetName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String postalCode;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String city;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String country;
 
     public EmployeeDTO(){
         this.id=-1;
     }
 
-    public EmployeeDTO(int id, String name, String address){
+    public EmployeeDTO(int id, String name){
         this.id=id;
         this.name=name;
-        this.address=address;
     }
 
-    public int getId(){
-        return this.id;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    public String adressLine1(){
+        return streetNumber + " " + streetName ;
     }
 
-    public String getName(){
-        return this.name;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    public String adressLine2(){
+        return city + " " + country + " " + postalCode;
     }
 
-    public String getAddress(){
-        return this.address;
+    public int getId() {
+        return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public String getName() {
+        return name;
     }
 
-    public void setAddress(String address){
-        this.address=address;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String toString(){
-        return "{id="+id+", name="+name+", address="+address+"}";
+        return "{id="+id+", name="+name+", address=" + adressLine1() + " " + adressLine2() + "}";
     }
 }
