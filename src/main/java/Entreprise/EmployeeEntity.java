8adef83f978e471ee50e027bf2ee42f30659db0e
RAFAEL AURIAC <rauriac@iut.univ-paris8.fr>
2020-01-23 15:15:30 +0100

    ignore

--- src/main/java/Entreprise/EmployeeEntity.java
+++ src/main/java/Entreprise/EmployeeEntity.java
@@ -1,14 +1,13 @@
 package Entreprise;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="Employee")
 public class EmployeeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String streetNumber;
